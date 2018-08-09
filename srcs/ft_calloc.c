e77f71296cc2cb3caaeaae63ef34199178462918
ashih <andyshih51@gmail.com>
2018-08-08 20:22:21 -0700

    added more prints for ft_calloc and ft_realloc

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ashih <ashih@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 21:04:30 by ashih             #+#    #+#             */
-/*   Updated: 2018/08/07 22:24:46 by ashih            ###   ########.fr       */
+/*   Updated: 2018/08/08 20:21:05 by ashih            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ void		*ft_calloc(size_t count, size_t size)
 {
 	void	*ret;
 
+	VERBOSE_PRINT("ft_calloc ( count=%lu , size=%lu )...\n", count, size);
 	ret = ft_malloc(count * size);
 	if (ret)
 		ft_bzero(ret, count * size);
+	VERBOSE_PRINT("ft_calloc ( count=%lu , size=%lu ) : ret\n",
+		count, size, ret);
 	return (ret);
 }
